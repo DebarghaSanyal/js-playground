1164689685e518239f8cbfec367920053e7f2ddf
DebarghaSanyal <debarghasanyal128@gmail.com>
2024-10-04 22:43:53 +0530

    Events in js

---
+++ event.js
@@ -0,0 +1,8 @@
+console.log("Hi! This is event!");
+
+
+// inline event handeling
+// onclick    single mouse click
+// ondblclick     double mouse click
+// onmouseover      hover
+// read more    https://developer.mozilla.org/en-US/docs/Web/API/Element#mouse_events
